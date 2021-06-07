7ec24abad2e66b5b6c765c112f5717425cf98c7d
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-07 21:37:18 +0800

    Add nodejs express server setup

---
+++ server/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
