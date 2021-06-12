7b04eeda24aa40e16fa8dc912365c556947cf2e5
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-12 18:26:28 +0800

    Add init admin account

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const { sequelize } = require('./models')
+const config = require('./config/config')
+const {User} = require('./models')
 
-const app = express();
+const app = express()
 
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
 
-app.post('/register', (req, res) => {
-    res.send({
-        // message: `Hello ${req.body.username}! You are successfully registered!`
-        message: req.body
-    });
-});
+require('./routes/index')(app)
 
-app.post('/login', (req, res) => {
-    res.send({
-        message: `Welcome back ${req.body.userName}!`
-    });
-});
+async function initAdmin (User) {
+    const user = await User.findOne({
+        where: {
+            username: 'admin'
+        }
+    })
 
+    if(!user) {
+        User.create({
+            "name": "admin",
+            "addressId": 0,
+            "email": "admin@gmail.com",
+            "type": "Admin",
+            "username": "admin",
+            "password": "Password123"
+        })
+    }
+}
 
-app.listen(process.env.PORT || 8081);
+sequelize.sync()
+    .then(() => {
+        app.listen(config.port)
+        initAdmin(User)
+        console.log(`Server started on port ${config.port}`)
+    })
