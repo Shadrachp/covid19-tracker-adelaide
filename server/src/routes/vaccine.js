5ac74c0abfa74c2a51a9b8b193df6d0187f56561
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-12 18:18:39 +0800

    Add vaccine endpoint

---
+++ server/src/routes/vaccine.js
@@ -0,0 +1,9 @@
+const VaccineController = require('../controllers/VaccineController')
+// const VaccineControllerPolicy = require('../policies/VaccineControllerPolicy')
+
+module.exports = (router) => {
+    router.post('/vaccine',
+        VaccineController.addVaccine)
+    router.get('/vaccine', VaccineController.getVaccine)
+    router.get('/vaccines', VaccineController.getVaccines)
+}
