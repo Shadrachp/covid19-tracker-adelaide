2103b43f62de7e15e739e28f299c2458389937a2
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-12 18:23:56 +0800

    Add vaccine controller

---
+++ server/src/controllers/VaccineController.js
@@ -0,0 +1,39 @@
+const {Vaccine} = require('../models')
+
+module.exports =  {
+    async addVaccine (req, res) {
+        try {
+            const vaccine = await Vaccine.create(req.body)
+            console.log(vaccine)
+            res.send(JSON.stringify(vaccine))
+        } catch(err) {
+            console.log(err)
+            res.status(400).send({
+                error: err
+            })
+        }
+    },
+
+    async getVaccine (req, res) {
+        try {
+            console.log(req.body.id)
+            const vaccine = await Vaccine.findOne({where: {id: req.body.id}})
+            res.send(JSON.stringify(vaccine))
+        } catch (err) {
+            res.status(400).send({
+                error: err
+            })
+        }
+    },
+
+    async getVaccines (req, res) {
+        try {
+            const vaccine = await Vaccine.findAll()
+            res.send(vaccine)
+        }catch(err) {
+            res.status(400).send({
+                error: err
+            })
+        }
+    }
+}
