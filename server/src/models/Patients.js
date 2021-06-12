179a477137b7edae782647bdaec54ab3d02c8522
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-12 18:20:31 +0800

    Add patients model

---
+++ server/src/models/Patients.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) =>
+ sequelize.define('Patients', {
+     id: {
+         type: DataTypes.INTEGER,
+         autoIncrement: true,
+         primaryKey: true
+     },
+     userId: {
+         type: DataTypes.INTEGER,
+         allowNull: true,
+         defaultValue: null
+     },
+     recovered: {
+         type: DataTypes.BOOLEAN,
+         allowNull: false,
+         required: true
+     }
+ })
