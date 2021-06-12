c270a2970a8c2460fec2e091fba78d35033bc7cf
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-12 18:21:05 +0800

    Add hospital model

---
+++ server/src/models/Hospital.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) =>
+ sequelize.define('Hospital', {
+     id: {
+         type: DataTypes.INTEGER,
+         autoIncrement: true,
+         primaryKey: true
+     },
+     patientsId: {
+         type: DataTypes.INTEGER,
+         required: true,
+         allowNull: false
+     },
+     vaccineId: {
+         type: DataTypes.INTEGER,
+         required: true,
+         allowNull: false
+     },
+     newCase: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         defaultValue: 0
+     },
+     case: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         defaultValue: 0
+     },
+     recovered: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         defaultValue: 0
+     },
+     address: {
+         type: DataTypes.STRING,
+         allowNull: false,
+     }
+ })
