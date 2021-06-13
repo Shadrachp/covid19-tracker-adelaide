cfc0214624f0b462a71fbf97aeef36a989563a4a
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-14 03:45:44 +0800

    Update check in history model with associations

--- server/src/models/CheckInHistory.js
+++ server/src/models/CheckInHistory.js
@@ -1,31 +1,42 @@
 const Sequelize = require('sequelize')
 
-module.exports = (sequelize, DataTypes) =>
- sequelize.define('CheckInHistory', {
-     id: {
-         type: DataTypes.INTEGER,
-         autoIncrement: true,
-         primaryKey: true
-     },
-     checkInCode: {
-         type: DataTypes.INTEGER,
-         required: true,
-         allowNull: false
-     },
-     venueId: {
-         type: DataTypes.INTEGER,
-         required: true,
-         allowNull: false
-     },
-     username: {
-         type: DataTypes.INTEGER,
-         required: true,
-         allowNull: false
-     },
-     address: DataTypes.STRING,
-     duration: DataTypes.INTEGER,
-     checkInTime: {
-         type: DataTypes.DATE,
-         defaultValue: Sequelize.NOW()
-     }
- })
+module.exports = (sequelize, DataTypes) => {
+  const CheckInHistory = sequelize.define('CheckInHistory', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    checkInCode: {
+      type: DataTypes.INTEGER,
+      required: true,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.INTEGER,
+      required: true,
+      allowNull: false
+    },
+    address: DataTypes.STRING,
+    checkInTime: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW()
+    }
+  })
+
+  CheckInHistory.associate = (models) => {
+    CheckInHistory.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    })
+
+    CheckInHistory.belongsTo(models.Venue, {
+      foreignKey: {
+        allowNull: false
+      }
+    })
+  }
+
+  return CheckInHistory
+}
