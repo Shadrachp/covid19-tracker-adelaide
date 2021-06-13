dcd5bac6de140eec7a066f016f32b3192e17f093
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-14 03:49:37 +0800

    Fix linting errors

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,61 +1,61 @@
-const {User} = require('../models')
+const { User } = require('../models')
 const jwt = require('jsonwebtoken')
 const config = require('../config/config')
 
 function jwtSignUser (user) {
-    const ONE_WEEK = 60*60*24*7
-    return jwt.sign(user, config.authentication.jwtSecret, {
-        expiresIn: ONE_WEEK
-    })
+  const ONE_WEEK = 60 * 60 * 24 * 7
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  })
 }
 
-module.exports =  {
-    async register (req, res) {
-        try {
-            const user = await User.create(req.body)
-            const userJson = user.toJSON()
-            console.log(userJson)
-            res.send({
-                user: userJson,
-                token: jwtSignUser(userJson)
-            })
-        } catch(err) {
-            console.log(err)
-            res.status(400).send({
-                error: err
-            })
-        }
-    },
-    async login (req, res) {
-        try {
-            const {username, password} = req.body
-            const user = await User.findOne({
-                where: {
-                    username: username
-                }
-            })
-
-            if(!user) {
-                return res.status(403).send({
-                    error: 'Login information is invalid'
-                })
-            }
-
-            const isPasswordMatch = await user.comparePassword(password)
-            if(!isPasswordMatch) {
-                return res.status(403).send({
-                    error: 'Login information is invalid'
-                })
-            }
-            const userJson = user.toJSON()
-            res.send({
-                user: userJson,
-                token: jwtSignUser(userJson)
-            })
-        } catch (err) {
-            res.status(400).send({
-                error: err
-            })
-        }
+module.exports = {
+  async register (req, res) {
+    try {
+      const user = await User.create(req.body)
+      const userJson = user.toJSON()
+      console.log(userJson)
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
+    } catch (err) {
+      console.log(err)
+      res.status(400).send({
+        error: err.errors[0].message
+      })
     }
+  },
+  async login (req, res) {
+    try {
+      const { username, password } = req.body
+      const user = await User.findOne({
+        where: {
+          username: username
+        }
+      })
+
+      if (!user) {
+        return res.status(403).send({
+          error: 'Login information is invalid'
+        })
+      }
+
+      const isPasswordMatch = await user.comparePassword(password)
+      if (!isPasswordMatch) {
+        return res.status(403).send({
+          error: 'Login information is invalid'
+        })
+      }
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
+    } catch (err) {
+      res.status(400).send({
+        error: err.errors[0].message
+      })
+    }
+  }
 }
--- server/src/controllers/HospitalController.js
+++ server/src/controllers/HospitalController.js
@@ -1,39 +1,39 @@
-const {Hospital} = require('../models')
+const { Hospital } = require('../models')
 
-module.exports =  {
-    async addHospital (req, res) {
-        try {
-            const hospital = await Hospital.create(req.body)
-            console.log(hospital)
-            res.send(JSON.stringify(hospital))
-        } catch(err) {
-            console.log(err)
-            res.status(400).send({
-                error: err
-            })
-        }
-    },
-
-    async getHospital (req, res) {
-        try {
-            console.log(req.body.id)
-            const hospital = await Hospital.findOne({where: {id: req.body.id}})
-            res.send(JSON.stringify(hospital))
-        } catch (err) {
-            res.status(400).send({
-                error: err
-            })
-        }
-    },
-
-    async getHospitals (req, res) {
-        try {
-            const hospital = await Hospital.findAll()
-            res.send(hospital)
-        }catch(err) {
-            res.status(400).send({
-                error: err
-            })
-        }
+module.exports = {
+  async addHospital (req, res) {
+    try {
+      const hospital = await Hospital.create(req.body)
+      console.log(hospital)
+      res.send(JSON.stringify(hospital))
+    } catch (err) {
+      console.log(err)
+      res.status(400).send({
+        error: err
+      })
     }
+  },
+
+  async getHospital (req, res) {
+    try {
+      console.log(req.body.id)
+      const hospital = await Hospital.findOne({ where: { id: req.body.id } })
+      res.send(JSON.stringify(hospital))
+    } catch (err) {
+      res.status(400).send({
+        error: err
+      })
+    }
+  },
+
+  async getHospitals (req, res) {
+    try {
+      const hospital = await Hospital.findAll()
+      res.send(hospital)
+    } catch (err) {
+      res.status(400).send({
+        error: err
+      })
+    }
+  }
 }
