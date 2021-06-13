191c0abbca7cc0136b8966eda5e0f8059c645364
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-14 03:51:50 +0800

    Fix linting errors

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const { sequelize } = require('./models')
 const config = require('./config/config')
-const {User} = require('./models')
+const { User } = require('./models')
 
 const app = express()
 
@@ -15,27 +15,27 @@ app.use(cors())
 require('./routes/index')(app)
 
 async function initAdmin (User) {
-    const user = await User.findOne({
-        where: {
-            username: 'admin'
-        }
-    })
-
-    if(!user) {
-        User.create({
-            "name": "admin",
-            "addressId": 0,
-            "email": "admin@gmail.com",
-            "type": "Admin",
-            "username": "admin",
-            "password": "Password123"
-        })
+  const user = await User.findOne({
+    where: {
+      username: 'admin'
     }
+  })
+
+  if (!user) {
+    User.create({
+      name: 'admin',
+      addressId: 0,
+      email: 'admin@gmail.com',
+      type: 'Admin',
+      username: 'admin',
+      password: 'Password123'
+    })
+  }
 }
 
-sequelize.sync()
-    .then(() => {
-        app.listen(config.port)
-        initAdmin(User)
-        console.log(`Server started on port ${config.port}`)
-    })
+sequelize.sync({})
+  .then(() => {
+    app.listen(config.port)
+    initAdmin(User)
+    console.log(`Server started on port ${config.port}`)
+  })
--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    port: process.env.PORT || 8081,
-    db: {
-        database: process.env.DB_NAME || 'covid19-tracker',
-        user: process.env.DB_USER || 'covid19-tracker',
-        password: process.env.DB_PASS || 'covid19-tracker',
-        options: {
-            dialect: process.env.DIALECT || 'sqlite',
-            host: process.env.HOST || 'localhost',
-            storage: './covid19-tracker.sqlite'
-        }
-    },
-    authentication: {
-        jwtSecret: process.env.JWT_SECRET || 'secret'
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'covid19-tracker',
+    user: process.env.DB_USER || 'covid19-tracker',
+    password: process.env.DB_PASS || 'covid19-tracker',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './covid19-tracker.sqlite'
     }
+  },
+  authentication: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
+  }
 }
