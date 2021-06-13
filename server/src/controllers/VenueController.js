ec2ec9a5286678de444568134a561a44ead9afed
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-14 03:47:33 +0800

    Reformat linting errors

--- server/src/controllers/VenueController.js
+++ server/src/controllers/VenueController.js
@@ -1,40 +1,39 @@
-const {Venue} = require('../models')
+const { Venue } = require('../models')
 
-
-module.exports =  {
-    async addVenue (req, res) {
-        try {
-            const venue = await Venue.create(req.body)
-            console.log(venue)
-            res.send(JSON.stringify(venue))
-        } catch(err) {
-            console.log(err)
-            res.status(400).send({
-                error: 'An error has occured trying to create a venue'
-            })
-        }
-    },
-
-    async getVenue (req, res) {
-        try {
-            console.log(req.body.id)
-            const venue = await Venue.findOne({where: {id: req.body.id}})
-            res.send(JSON.stringify(venue))
-        } catch (err) {
-            res.status(400).send({
-                error: 'An error has occured trying to fetch a venue'
-            })
-        }
-    },
-
-    async getVenues (req, res) {
-        try {
-            const venue = await Venue.findAll()
-            res.send(venue)
-        }catch(err) {
-            res.status(400).send({
-                error: 'An error has occured trying to fetch the venues'
-            })
-        }
+module.exports = {
+  async addVenue (req, res) {
+    try {
+      const venue = await Venue.create(req.body)
+      console.log(venue)
+      res.send(JSON.stringify(venue))
+    } catch (err) {
+      console.log(err)
+      res.status(400).send({
+        error: 'An error has occured trying to create a venue'
+      })
     }
+  },
+
+  async getVenue (req, res) {
+    try {
+      console.log(req.body.id)
+      const venue = await Venue.findOne({ where: { id: req.body.id } })
+      res.send(JSON.stringify(venue))
+    } catch (err) {
+      res.status(400).send({
+        error: 'An error has occured trying to fetch a venue'
+      })
+    }
+  },
+
+  async getVenues (req, res) {
+    try {
+      const venue = await Venue.findAll()
+      res.send(venue)
+    } catch (err) {
+      res.status(400).send({
+        error: 'An error has occured trying to fetch the venues'
+      })
+    }
+  }
 }
