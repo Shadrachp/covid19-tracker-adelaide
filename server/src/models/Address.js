b73f30c46915b31067042855c460bcad04c6e54f
Shadrach Penano <shadrachgabriel@icloud.com>
2021-06-14 03:47:11 +0800

    Add address association

--- server/src/models/Address.js
+++ server/src/models/Address.js
@@ -1,38 +1,38 @@
 module.exports = (sequelize, DataTypes) => {
- const Address = sequelize.define('Address', {
-     id: {
-         type: DataTypes.INTEGER,
-         autoIncrement: true,
-         primaryKey: true
-     },
-     accountType: {
-         type: DataTypes.STRING,
-         required: true,
-         allowNull: false
-     },
-     location: {
-         type: DataTypes.STRING,
-         required: true
-     },
- })
+  const Address = sequelize.define('Address', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    accountType: {
+      type: DataTypes.STRING,
+      required: true,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      required: true
+    }
+  })
 
- Address.associate = (models) => {
-     Address.belongsTo(models.User, {
-         foreignKey: {
-             allowNull: true,
-         }
-         // sourceKey: 'checkInCode',
-         // targetKey: 'id'
-     })
+  Address.associate = (models) => {
+    Address.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: true
+      }
+      // sourceKey: 'checkInCode',
+      // targetKey: 'id'
+    })
 
-     Address.belongsTo(models.Venue, {
-         foreignKey: {
-             allowNull: true,
-         }
-         // sourceKey: 'checkInCode',
-         // targetKey: 'id'
-     })
- }
+    Address.belongsTo(models.Venue, {
+      foreignKey: {
+        allowNull: true
+      }
+      // sourceKey: 'checkInCode',
+      // targetKey: 'id'
+    })
+  }
 
- return Address
+  return Address
 }
